51139034948905fc973eb89a25236511d0e9388f
Matt Gasper <mrgasper27@gmail.com>
2019-02-28 10:20:10 -0600

    Created initial sign in page

---
+++ src/pages/Sign_In/Sign_In.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Button, Col, Container, Form, Row, Table } from 'react-bootstrap';
+import firebase from '../../firebase.js';
+
+class SignIn extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+        username: '',
+        password: ''
+        }
+    }
+
+    
+
+
+
+    render() {
+        return(
+            
+            <Container>
+                
+                <Row>
+                    <Col>You need to sign in or sign up before continuing.</Col>
+                </Row>
+
+                {/* Sign in box */}
+                <Row>
+                    <Col>
+                        {/* Header */}
+                        <Row>
+                            <Col>Sign In</Col>
+                        </Row>
+
+                        {/* Sign in form */}
+                        <Row>
+                            <Col>
+                                <Form>
+                                    <Form.Group controlId="formBasicEmail">
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control type="email" placeholder="tiny@foundrymakes.com" />
+                                    </Form.Group>
+
+                                    <Form.Group controlId="formBasicPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type="password" placeholder="Password" />
+                                    </Form.Group>
+                                
+                                    <Button variant="danger" size='lg' type="submit">
+                                        Sign In
+                                    </Button>
+                                </Form>;
+                            </Col>
+                        </Row>
+
+                        {/* Sign up link */}
+                        <Row>
+                            <Col>Sign Up</Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+            
+        );
+    }
+}
+
+export default SignIn;
