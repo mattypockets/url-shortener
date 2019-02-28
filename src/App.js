5e9e9f57e4279dfd58528d309fa5cab563ef2490
Matt Gasper <mrgasper27@gmail.com>
2019-02-28 10:29:36 -0600

    Exported sign in and added to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import SignIn from './pages/Sign_In';
 import Main from './pages/Main';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Main />
+      <div>
+        <SignIn />
+        <Main />
+      </div>
     );
   }
 }
